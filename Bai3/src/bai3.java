b2969f83f2e17e6538b735914f51c6e0bc5310bf
tranhung104 <tranhungvip01@gmail.com>
2026-02-06 22:39:57 +0700

    bai3

---
+++ Bai3/src/bai3.java
@@ -0,0 +1,28 @@
+
+public class bai3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double weight = 65.5;    // kg
+        double height = 1.70;    // m
+        
+        double bmi = weight / (height * height);
+        
+        System.out.println("Cân nặng: " + weight + " kg");
+        System.out.println("Chiều cao: " + height + " m");
+        System.out.println("Chỉ số BMI: " + bmi);
+        
+        // Phân loại BMI
+        if (bmi < 18.5) {
+            System.out.println("Phân loại: Thiếu cân");
+        } else if (bmi < 25) {
+            System.out.println("Phân loại: Bình thường");
+        } else if (bmi < 30) {
+            System.out.println("Phân loại: Thừa cân");
+        } else {
+            System.out.println("Phân loại: Béo phì");
+        }
+
+	}
+
+}
