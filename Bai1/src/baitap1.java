1a630c3d428466b22516ee95ad41710aae11946e
tranhung104 <tranhungvip01@gmail.com>
2026-02-06 22:32:34 +0700

    baitap1

---
+++ Bai1/src/baitap1.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class baitap1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int a = 15;
+	        int b = 4;
+	        double c = 3.5;
+	        
+	        int tong = a + b;
+	        double thuong = a / c;
+	        int phanDu = a % (int)c;
+	       
+	        System.out.println("Tổng: " + tong);
+	        System.out.println("Thương: " + thuong);
+	        System.out.println("Phần dư: " + phanDu);
+	}
+
+}
