64e255ea8d65b2db79ca72e4bfeb14b1f676ca9d
tranhung104 <tranhungvip01@gmail.com>
2026-02-06 22:26:39 +0700

    vi du dau tien
    demo

---
+++ src/tiil/edu/xinchao.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package tiil.edu;
+
+/**
+ * 
+ */
+public class xinchao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chao toi la cac sv khoa 66 cntt - NTU: hoc lap trinh java");
+		System.out.print("Trần Gia Hưng");
+
+	}
+
+}
