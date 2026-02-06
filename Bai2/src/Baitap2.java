37cc2fecca6613a6531296fd84233ecb48e560ee
tranhung104 <tranhungvip01@gmail.com>
2026-02-06 22:38:39 +0700

    bai2

---
+++ Bai2/src/Baitap2.java
@@ -0,0 +1,12 @@
+
+public class Baitap2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double celsius = 25.0;
+        double fahrenheit = (celsius * 9/5) + 32;
+        
+        System.out.println(celsius + "°C = " + fahrenheit + "°F");
+	}
+
+}
