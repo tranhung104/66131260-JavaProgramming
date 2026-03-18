6713957c4a3e4de47ff0536b7aa25247cb738715
tranhung104 <tranhungvip01@gmail.com>
2026-03-18 22:55:33 +0700

    vd2

--- vd2_arraylist/src/vd2_arraylist/vd2.java
+++ vd2_arraylist/src/vd2_arraylist/vd2.java
@@ -25,3 +25,41 @@ public class vd2 {
 	        return String.format("Mã: %s | Họ tên: %s | Điểm TB: %.2f",  maSV, hoTen, diemTB);
 	    }
 	}
+//Lớp quản lý sinh viên sử dụng ArrayList
+class QuanLySinhVien {
+private List<vd2> danhSachSV;
+
+public QuanLySinhVien() {
+   danhSachSV = new ArrayList<>();
+}
+
+// Thêm sinh viên - add()
+public void themSinhVien(vd2 sv) {
+   danhSachSV.add(sv);
+   System.out.println("Đã thêm: " + sv.getHoTen());
+}
+
+// Tìm sinh viên theo mã - sử dụng get() và vòng lặp
+public vd2 timTheoMa(String maSV) {
+   for (vd2 sv : danhSachSV) {
+       if (sv.getMaSV().equals(maSV)) {
+           return sv;
+       }
+   }
+   return null;
+}
+
+// Tìm sinh viên có điểm cao nhất
+public vd2 timDiemCaoNhat() {
+   if (danhSachSV.isEmpty()) {
+       return null;
+   }
+   
+   vd2 svCaoNhat = danhSachSV.get(0);
+   for (vd2 sv : danhSachSV) {
+       if (sv.getDiemTB() > svCaoNhat.getDiemTB()) {
+           svCaoNhat = sv;
+       }
+   }
+   return svCaoNhat;
+}
