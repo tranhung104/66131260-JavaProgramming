def99b6e0b5b7f9b67f8d02ea032cb05692b161e
tranhung104 <tranhungvip01@gmail.com>
2026-03-18 23:02:07 +0700

    arraylist_songuyen

--- songuyen/src/songuyen/arraylist_songuyen.java
+++ songuyen/src/songuyen/arraylist_songuyen.java
@@ -1,5 +1,37 @@
 package songuyen;
 
-public class arraylist_songuyen {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class arraylist_songuyen {
+    public static void main(String[] args) {
+        ArrayList<Integer> danhSachSoNguyen = new ArrayList<>();
+        Scanner banPhim = new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Mời nhập phần tử thứ " + i + ": ");
+            int tam = banPhim.nextInt();
+            danhSachSoNguyen.add(tam);
+        }
+
+        System.out.println("Danh sách vừa nhập là: ");
+        for (Integer x : danhSachSoNguyen) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+
+        int soLuongChan = 0;
+        for (Integer x : danhSachSoNguyen) {
+            if (x % 2 == 0) {
+                soLuongChan++;
+            }
+        }
+        System.out.println("Số phần tử chẵn là: " + soLuongChan);
+
+        int tong = 0;
+        for (Integer x : danhSachSoNguyen) {
+            tong += x;
+        }
+        System.out.println("Tổng tất cả các phần tử là: " + tong);
+    }
 }
