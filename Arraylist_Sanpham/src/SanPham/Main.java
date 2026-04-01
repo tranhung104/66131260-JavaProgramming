fc1db9993d1390884a8cf8a07dbfe1ce86085dbf
tranhung104 <tranhungvip01@gmail.com>
2026-04-01 21:35:32 +0700

    SanPham

--- Arraylist_Sanpham/src/SanPham/Main.java
+++ Arraylist_Sanpham/src/SanPham/Main.java
@@ -2,4 +2,19 @@ package SanPham;
 
 public class Main {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SanPham sp1 = new SanPham();
+        
+        System.out.println("Mời nhập thông tin sản phẩm:");
+        sp1.nhap();
+        
+        System.out.println("\nThông tin sản phẩm vừa nhập:");
+        sp1.xuat();
+        
+        SanPham sp2 = new SanPham("Laptop", 25000000, 123456);
+        System.out.println("\nThông tin sản phẩm có sẵn:");
+        sp2.xuat();
+	}
+
 }
--- Arraylist_Sanpham/src/SanPham/SanPham.java
+++ Arraylist_Sanpham/src/SanPham/SanPham.java
@@ -1,5 +1,47 @@
 package SanPham;
+import java.util.Scanner;
 
 public class SanPham {
+    private String tenSp;
+    private double donGia;
+    private double giamGia;
 
+    public SanPham() {}
+
+    public SanPham(String tenSp, double donGia, double giamGia) {
+        this.tenSp = tenSp;
+        this.donGia = donGia;
+        this.giamGia = giamGia;
+    }
+
+    public String getTenSp() { return tenSp; }
+    public void setTenSp(String tenSp) { this.tenSp = tenSp; }
+
+    public double getDonGia() { return donGia; }
+    public void setDonGia(double donGia) { this.donGia = donGia; }
+
+    public double getGiamGia() { return giamGia; }
+    public void setGiamGia(double giamGia) { this.giamGia = giamGia; }
+
+    public double getThueNhapKhau() {
+        return 0.1 * donGia;
+    }
+
+    public void nhap() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập tên sản phẩm: ");
+        this.tenSp = scanner.nextLine();
+        System.out.print("Nhập đơn giá: ");
+        this.donGia = Double.parseDouble(scanner.nextLine());
+        System.out.print("Nhập giảm giá: ");
+        this.giamGia = Double.parseDouble(scanner.nextLine());
+    }
+
+    public void xuat() {
+        System.out.println("----------------------------");
+        System.out.println("Tên sản phẩm: " + this.tenSp);
+        System.out.println("Đơn giá: " + this.donGia);
+        System.out.println("Giảm giá: " + this.giamGia);
+        System.out.println("Thuế nhập khẩu: " + getThueNhapKhau());
+    }
 }
