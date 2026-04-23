00d056364a089b26ec76d3aa4634d3ff747e0eba
tranhung104 <tranhungvip01@gmail.com>
2026-04-23 08:04:54 +0700

    Lần 4

--- thigk2.trangiahung/src/thigk2/trangiahung/Cau2.java
+++ thigk2.trangiahung/src/thigk2/trangiahung/Cau2.java
@@ -1,35 +1,17 @@
 package thigk2.trangiahung;
 
-public class Cau2 {
-	private String MaSv;
-	private String hoten;
-	private int namsinh;
-	private String lop;
-	
-	public Cau2() {}
-	
-	public Cau2(String MaSv, String hoten, int namsinh, String lop) {
-		this.MaSv = MaSv;
-		this.hoten = hoten;
-		this.namsinh = namsinh;
-		this.lop = lop;
-	}
-	
-	public String getMaSV() { return MaSv;}
-	public void setMaSV(String MaSv) { this.MaSv = MaSv; }
-	
-	public String gethoten() {return hoten;}
-	public void sethoten(String hoten) {this.hoten = hoten; }
+import java.util.ArrayList;
+import java.util.Scanner;
 
-	public int getnamsinh() { return namsinh;}
-	public void setnamsinh(int namsinh) { this.namsinh = namsinh; }
-	
-	public String getlop() { return lop;}
-	public void setlop(String lop) { this.lop = lop; }
-	
-	@override
-	public String toString() {
-		return MaSv +
+public class Cau2 {
+	public static void main(String[] args) {
+		ArrayList<Cau2> danhsach = new ArrayList <>();
+		
+		danhsach.add(new SinhVien("SV001", "Tran Van A", 2003, "63-CNNT-2"));
+		danhsach.add(new SinhVien("SV002", "Tran Van B", 2004, "64-CNNT-2"));
+		danhsach.add(new SinhVien("SV003", "Tran Gia Hung", 2006, "66-CNNT-2"));
+		
+		System.out
 	}
 
 }
