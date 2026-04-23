2750158759707e4427dacea434d83d8a085d2295
tranhung104 <tranhungvip01@gmail.com>
2026-04-23 07:44:25 +0700

    Lần 2

--- thigk2.trangiahung/src/thigk2/trangiahung/Cau1.java
+++ thigk2.trangiahung/src/thigk2/trangiahung/Cau1.java
@@ -1,5 +1,29 @@
 package thigk2.trangiahung;
 
-public class Cau1 {
+import java.util.Scanner;
 
+public class Cau1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhập Chiều cao (m): ");
+		double chieucao = sc.nextDouble();
+		
+		System.out.print("Nhập cân nặng (kg): ");
+		double cannang= sc.nextDouble();
+		
+		double bmi = cannang / (chieucao * chieucao);
+		System.out.printf("Chỉ số bmi: %.2f%n", bmi);
+		
+		String tinhtrang;
+		if (bmi < 18.5) {
+			tinhtrang = "Thiếu cân";
+		} else if (bmi < 24.9) {
+			tinhtrang = "Bình thường";
+		} else if (bmi < 29.9) {
+			tinhtrang = "Thừa cân";
+		} else {
+			tinhtrang = "Béo pfih";
+		}
+		
+	}
 }
