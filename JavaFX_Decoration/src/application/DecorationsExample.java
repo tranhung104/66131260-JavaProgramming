1479ab084aa8905798ea9b2f0ffd3b191fc97eb1
tranhung104 <tranhungvip01@gmail.com>
2026-05-14 00:36:33 +0700

    Update DecorationsExample.java

--- JavaFX_Decoration/src/application/DecorationsExample.java
+++ JavaFX_Decoration/src/application/DecorationsExample.java
@@ -1,48 +1,33 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.stage.Stage;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 
 public class DecorationsExample extends Application {
     @Override
     public void start(Stage stage) {
-        Text text1 = new Text("Email");
-        Text text2 = new Text("Password");
-        TextField textField1 = new TextField();
-        PasswordField textField2 = new PasswordField();
-        Button button1 = new Button("Submit");
-        Button button2 = new Button("Clear");
+        Text text1 = new Text("Hi how are you");
+        text1.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.ITALIC, 20));
+        text1.setX(50);
+        text1.setY(75);
+        text1.setStrikethrough(true);
 
-        GridPane gridPane = new GridPane();
-        gridPane.setMinSize(400, 200);
-        gridPane.setPadding(new Insets(10, 10, 10, 10));
-        gridPane.setVgap(5);
-        gridPane.setHgap(5);
-        gridPane.setAlignment(Pos.CENTER);
-        gridPane.add(text1, 0, 0);
-        gridPane.add(textField1, 1, 0);
-        gridPane.add(text2, 0, 1);
-        gridPane.add(textField2, 1, 1);
-        gridPane.add(button1, 0, 2);
-        gridPane.add(button2, 1, 2);
+        Text text2 = new Text("Welcome to Tutorialspoint");
+        text2.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.ITALIC, 20));
+        text2.setX(50);
+        text2.setY(150);
+        text2.setUnderline(true);
 
-        button1.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;");
-        button2.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;");
-        text1.setStyle("-fx-font: normal bold 20px 'serif'");
-        text2.setStyle("-fx-font: normal bold 20px 'serif'");
-        gridPane.setStyle("-fx-background-color: BEIGE;");
-
-        Scene scene = new Scene(gridPane);
-        stage.setTitle("CSS Example");
+        Group root = new Group(text1, text2);
+        Scene scene = new Scene(root, 600, 300);
+        stage.setTitle("Decorations Example");
         stage.setScene(scene);
         stage.show();
     }
